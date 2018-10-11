97e6d12177bd19c93e36703a59a3f9ec78907ef7
Gizem <gizemny@gmail.com>
2018-10-11 13:02:46 -0500

    more changes

--- FEND/project7/src/components/ListItem.js
+++ FEND/project7/src/components/ListItem.js
@@ -6,11 +6,15 @@ export default class ListItem extends Component {
       <li 
         className='listItem' 
         onClick={()=> this.props.onListItemClick(this.props)}>
-        <img src={
-          this.props.categories[0].icon.prefix + "32" + 
-          this.props.categories[0].icon.suffix
-          } alt="Venue category icon"/>
+        <img 
+          src={
+           this.props.categories[0].icon.prefix + "32" + 
+           this.props.categories[0].icon.suffix
+           } 
+          alt="Venue category icon"/>
       {this.props.name}
+      {/* <img alt="venue" src={`${this.props.bestPhoto.prefix}200x200${this.props.bestPhoto.suffix}`}/> */}
+      <p>{this.props.location.formattedAddress}</p>
       </li>
     );
   }
